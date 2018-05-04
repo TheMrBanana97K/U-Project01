91cd24b202a0f05278ecfadcfb705c7fafad81c7
Kacper Kruczek <neronim66@gmail.com>
2018-05-05 01:24:51 +0200

    Simple movement

--- Assets/Scripts/Player/Movement/Movement.cs
+++ Assets/Scripts/Player/Movement/Movement.cs
@@ -2,15 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Player))]
 public class Movement : MonoBehaviour {
+    Player player;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    Rigidbody rb;
+    Vector3 velocity;
+    public void Start()
+    {
+        player = gameObject.GetComponent<Player>();
+
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+    public void Update()
+    {
+        Vector3 direction = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+        velocity = direction * player.speed.getValue();
+    }
+    private void FixedUpdate()
+    {
+        Move(rb.position+velocity * Time.deltaTime);
+    }
+    void Move(Vector3 direction)
+    {
+        rb.MovePosition(direction);
+    }
 }
