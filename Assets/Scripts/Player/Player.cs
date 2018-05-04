7ee5c1cccb2880c6f0b2b97020d0203041e93466
Kacper Kruczek <neronim66@gmail.com>
2018-05-05 01:01:54 +0200

    PlayerStats

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -4,25 +4,23 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-    public Statistic healthStat;
+    [Header("Player Stats")]
+    public Statistic health=new Statistic(0f,100f,1000f);
+    public Statistic energy=new Statistic(0f,100f,1000f);
 
+    public System.Action OnDeath;
 
-	// Use this for initialization
-	void Start () {
-        StartCoroutine(DecreaseHealth());
+    // Use this for initialization
+    void Start () {
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
-	}
-    IEnumerator DecreaseHealth()
-    {
-        while (healthStat.getValue()>0)
+        if (health.getValue() < 0)
         {
-            healthStat.plainValue -= 1;
-            yield return new WaitForSeconds(1);
+            if (OnDeath != null) OnDeath();
         }
-        print("Dead");
-    }
+	}
+
 }
