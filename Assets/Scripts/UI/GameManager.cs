4d1dc003d311f0e31c55a7c898bfd8a4a705bf44
Kacper Kruczek <neronim66@gmail.com>
2018-05-05 01:52:44 +0200

    simple ui

---
+++ Assets/Scripts/UI/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GameManager : MonoBehaviour {
+    Player player;
+    public GameObject GameOver;
+    public GameObject Instructions;
+    public Text HP;
+    bool isGameOver;
+    // Use this for initialization
+    void Start () {
+      player=  FindObjectOfType<Player>();
+        player.OnDeath += OnGameOver;
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        HP.text = player.health.getValue().ToString();
+        if (isGameOver)
+        {
+
+        }
+	}
+    void OnGameOver()
+    {
+        isGameOver = true;
+        GameOver.SetActive(true);
+        Instructions.SetActive(false);
+    }
+}
